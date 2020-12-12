bf0df9b5684cc76b89d7f8f6caca58400fe9806c
Salome Kavtaradze <s_kavtaradze2@cu.edu.ge>
2020-12-12 23:53:29 +0400

    Optimize url code

--- app/src/main/java/com/example/movies/api/JsonApi.kt
+++ app/src/main/java/com/example/movies/api/JsonApi.kt
@@ -2,9 +2,25 @@ package com.example.movies.api
 
 import com.example.movies.objects.MoviesInfo
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface JsonApi {
     @GET("movies-db/db")
     fun getMovies(): Call<MoviesInfo>
+
+    companion object {
+
+        var BASE_URL = "https://my-json-server.typicode.com/nikoloz14/"
+
+        fun create(): JsonApi {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+
+            return retrofit.create(JsonApi::class.java)
+        }
+    }
 }
--- app/src/main/java/com/example/movies/movies/MovieItemFragment.kt
+++ app/src/main/java/com/example/movies/movies/MovieItemFragment.kt
@@ -8,33 +8,25 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movies.R
 import com.example.movies.adapters.MovieItemAdapter
 import com.example.movies.api.JsonApi
-import com.example.movies.objects.Movies
 import com.example.movies.objects.MoviesInfo
 import kotlinx.android.synthetic.main.movie_item_fragment.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MovieItemFragment : Fragment(R.layout.movie_item_fragment) {
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("https://my-json-server.typicode.com/nikoloz14/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private val jsonApi = retrofit.create(JsonApi::class.java)
-    private var call = jsonApi.getMovies()
+    private val listOfMovies = ArrayList<MoviesInfo>()
+    private var jsonApi = JsonApi.create().getMovies()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        call.enqueue(object : Callback<MoviesInfo> {
+        movie_item.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        movie_item.adapter = MovieItemAdapter(listOfMovies)
+
+        jsonApi.enqueue(object : Callback<MoviesInfo> {
             override fun onResponse(call: Call<MoviesInfo>, response: Response<MoviesInfo>) {
-                val listOfMovies = ArrayList<MoviesInfo>()
-                movie_item.layoutManager =
-                    LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-                movie_item.adapter = MovieItemAdapter(listOfMovies)
                 Log.d("Successssssssssss", response.body().toString())
             }
 
