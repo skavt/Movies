f4994313b84c43f732ad6852cb93ee32f3b7f5b7
skavt <saliikavt@gmail.com>
2020-12-15 23:14:17 +0400

    TODO uncomment code. everything works fine

---
+++ app/src/main/java/com/example/movies/adapters/CastAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.movies.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movies.R
+import com.example.movies.objects.Cast
+import kotlinx.android.synthetic.main.cast_item.view.*
+
+class CastAdapter(
+    private val castList: ArrayList<Cast>
+) :
+    RecyclerView.Adapter<CastAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.cast_item, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        holder.setContent(castList[position])
+        holder.setContent()
+    }
+
+    //    override fun getItemCount(): Int = castList.size
+    override fun getItemCount(): Int = 1
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        //        fun setContent(cast: Cast) {
+        fun setContent() {
+//            with(cast) {
+            itemView.full_name.text = "fullName"
+            itemView.role_name.text = "role"
+//                Glide.with(itemView.context).load(imageUrl).into(itemView.cast_image)
+//            }
+        }
+    }
+}
--- app/src/main/java/com/example/movies/details/CastFragment.kt
+++ app/src/main/java/com/example/movies/details/CastFragment.kt
@@ -1,7 +1,22 @@
 package com.example.movies.details
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.movies.R
+import com.example.movies.adapters.CastAdapter
+import com.example.movies.objects.Cast
+import kotlinx.android.synthetic.main.cast_fragment.*
 
 class CastFragment : Fragment(R.layout.cast_fragment) {
+
+    private val listOfCast = ArrayList<Cast>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cast_item.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        cast_item.adapter = CastAdapter(listOfCast)
+    }
 }
