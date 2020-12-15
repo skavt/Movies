ce81b0b00223ddaad726f72969fb43ce63179b2b
skavt <saliikavt@gmail.com>
2020-12-15 19:57:30 +0400

    Add tab layout into view pager

--- app/src/main/java/com/example/movies/details/MovieDetailedActivity.kt
+++ app/src/main/java/com/example/movies/details/MovieDetailedActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.movies.R
 import com.example.movies.adapters.MovieDetailedAdapter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_movie_detailed.*
 
 class MovieDetailedActivity : AppCompatActivity() {
@@ -13,8 +14,12 @@ class MovieDetailedActivity : AppCompatActivity() {
         setContentView(R.layout.activity_movie_detailed)
 
         val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+
         viewPager.adapter = MovieDetailedAdapter(supportFragmentManager)
-        tabLayout!!.addTab(tabLayout!!.newTab().setText("Info"))
-        tabLayout!!.addTab(tabLayout!!.newTab().setText("Cast"))
+
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout!!.addTab(tabLayout.newTab().setText("Info"))
+        tabLayout.addTab(tabLayout.newTab().setText("Cast"))
     }
 }
